76d8be720613b84e35f9202b38078e5242d0fdaa
coderAnwi <90803165+coderAnwi@users.noreply.github.com>
2022-11-27 13:23:40 -0700

    Add files via upload

---
+++ Project 77/main.js
@@ -0,0 +1,14 @@
+var first_name = "Allu";
+console.log(first_name);
+
+var last_name = "Arjun";
+console.log(last_name);
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+
+var add = a+b;
+console.log(add);
